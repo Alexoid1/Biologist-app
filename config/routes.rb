49a22cc5ba0fb08ffe47190778466473d5553cfa
alexoid <alex_zam@hotmail.es>
2020-10-18 15:31:56 -0500

    add user info

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "opinions#index"
+  root "users#new"
 end
