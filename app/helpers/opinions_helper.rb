e3bea8a21b335d542e4f9e8b13ffc83004e61ead
alexoid <alex_zam@hotmail.es>
2020-10-22 12:30:18 -0500

    destroy button just show in the current_user opinions

--- app/helpers/opinions_helper.rb
+++ app/helpers/opinions_helper.rb
@@ -4,4 +4,12 @@ module OpinionsHelper
 
     content_tag :p, "Post could not be saved. #{post.errors.full_messages.join('. ')}", class: 'errors'
   end
+
+  def destroy_opinion(opinion, current_user)
+    return unless opinion.user_id == current_user.id
+
+    content_tag(:p, class: 'float-right') do
+      link_to 'Destroy', opinion, method: :delete, data: { confirm: 'Are you sure?' }
+    end
+  end
 end
